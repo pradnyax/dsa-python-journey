bd9c3330ba57143ba17414263a3f7fdf4e3e1e1f
Pradnya Jeurkar <pradnya.ce@gmail.com>
2026-04-20 19:06:45 +0530

    add 01-DLL-Constructor.py to 04_Doubly_Linkedlist folder

---
+++ 01_Udemy_ScottBarrett_dsa/04_Doubly_Linkedlist/01-DLL-Constructor.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+my_dll = DoublyLinkedList(7)
+
+my_dll.print_list()
+
+
+# for my understanding-
+"""
+{
+"value": 7,
+"next": None,
+"prev": None
+}
+"""
